1a92563dddff0d7749f18844ecf00ac78e6f4823
Andrew Taylor <rolyata47@gmail.com>
2025-11-15 10:56:25 -0600

    Standardizing command line arguments across different files

--- keygen.py
+++ keygen.py
@@ -1,18 +1,18 @@
 import random
 import argparse
 
-def gen_key(key_length):
+def create_key(key_length):
     key = [chr(random.randint(0, 255)) for i in range(0, key_length)]
-    return "".join(key)
+    return ''.join(key)
 
 def main():
-    parser = argparse.ArgumentParser(prog="keygen.py", description="Create an XOR key")
-    parser.add_argument("-kl", "--keylength", type=int, default=64)
-    parser.add_argument("-o", "--output", type=str, default="key.txt")
+    parser = argparse.ArgumentParser(prog='keygen.py', description='Create an XOR key')
+    parser.add_argument('-kl', '--keylength', type=int, default=64)
+    parser.add_argument('-o', '--outputfile', type=str, default='key.txt')
     args = parser.parse_args()
-    key = gen_key(args.keylength)
-    with open(args.output, "w") as file:
+    key = create_key(args.keylength)
+    with open(args.outputfile, 'w') as file:
         file.write(key)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- xor.py
+++ xor.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import os
 import argparse
 
 def crypt(msg, key):
@@ -9,31 +8,26 @@ def crypt(msg, key):
     msgcodes = list(map(lambda x: ord(x), msg))
     keycodes = list(map(lambda x: ord(x), key))
     out = [chr(msgcodes[i] ^ keycodes[i % keylen]) for i in range(0, msglen)]
-    return "".join(out)
+    return ''.join(out)
 
-def main():
-    home_dir = os.path.expanduser("~")
-    default_key = f"{home_dir}/keys/xor.txt"
-    parser = argparse.ArgumentParser(prog="xor.py", description="Encrypt or decrypt a message")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='xor.py', description='Encrypt or decrypt a message')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("message", type=str, nargs="?")
-    group.add_argument("-m", "--msgfile", type=str)
-    parser.add_argument("-k", "--keyfile", type=str, default=default_key)
-    parser.add_argument("-o", "--output", type=str)
+    group.add_argument('message', type=str, nargs='?')
+    group.add_argument('-i', '--inputfile', type=str)
+    parser.add_argument('-k', '--keyfile', type=str, default='key.txt')
+    parser.add_argument('-o', '--outputfile', type=str)
     args = parser.parse_args()
-    if args.msgfile:
-        with open(args.msgfile, "r") as msgfile:
-            msg = msgfile.read()
+    if args.inputfile:
+        with open(args.inputfile, 'r') as file:
+            msg = file.read()
     else:
         msg = args.message
-    with open(args.keyfile, "r") as keyfile:
-        key = keyfile.read()
+    with open(args.keyfile, 'r') as file:
+        key = file.read()
     output = crypt(msg, key)
-    if args.output:
-        with open(args.output, "w") as outputfile:
-            outputfile.write(output)
+    if args.outputfile:
+        with open(args.outputfile, 'w') as file:
+            file.write(output)
     else:
-        print(output, end="")
-    
-if __name__ == "__main__":
-    main()
+        print(output, end='')
