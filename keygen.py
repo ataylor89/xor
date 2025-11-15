8355a095084cd6a074b8df0827235d55deb540e3
Andrew Taylor <rolyata47@gmail.com>
2025-11-15 10:58:24 -0600

    Small changes

--- keygen.py
+++ keygen.py
@@ -5,7 +5,7 @@ def create_key(key_length):
     key = [chr(random.randint(0, 255)) for i in range(0, key_length)]
     return ''.join(key)
 
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='keygen.py', description='Create an XOR key')
     parser.add_argument('-kl', '--keylength', type=int, default=64)
     parser.add_argument('-o', '--outputfile', type=str, default='key.txt')
@@ -13,6 +13,3 @@ def main():
     key = create_key(args.keylength)
     with open(args.outputfile, 'w') as file:
         file.write(key)
-
-if __name__ == '__main__':
-    main()
