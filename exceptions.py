c703ae9b68b18c5f9cddb8884d0bd56a3d53ef02
Andrew Taylor <rolyata47@gmail.com>
2026-01-27 20:07:14 -0600

    Improving error handling

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,14 @@
 class InvalidKeyError(Exception):
 
     def __init__(self, message='Invalid key'):
-        super().__init__('InvalidKeyError: %s' %message)
+        super().__init__(message)
 
 class InvalidThresholdError(Exception):
 
-    def __init__(self, message='The min and max values must be in the range [0, 0x110000)'):
-        super().__init__('InvalidThresholdError: %s' %message)
+    def __init__(self, message='Invalid threshold'):
+        super().__init__(message)
 
 class InvalidKeyLengthError(Exception):
 
-    def __init__(self, message='The key length must be a positive integer'):
-        super().__init__('InvalidThresholdError: %s' %message)
+    def __init__(self, message='Invalid key length'):
+        super().__init__(message)
