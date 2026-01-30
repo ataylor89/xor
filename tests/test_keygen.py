dbed6e071f29d70e68be46e446cc22a4241cf0d0
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 18:47:15 -0600

    Renaming method from process_params to verify_key

--- tests/test_keygen.py
+++ tests/test_keygen.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class TestKeygen(TestCase):
 
-    def process_params(self, keylength, tmin, tmax):
+    def verify_key(self, keylength, tmin, tmax):
         key = keygen.create_key(keylength, tmin, tmax)
         assert len(key) == keylength
         for value in key:
@@ -11,10 +11,10 @@ class TestKeygen(TestCase):
             assert value >= tmin and value <= tmax
 
     def test_keygen(self):
-        self.process_params(64, 0, 255)
+        self.verify_key(64, 0, 255)
     
     def test_keygen_with_large_keylength(self):
-        self.process_params(1028, 0, 255)
+        self.verify_key(1028, 0, 255)
 
     def test_keygen_with_large_keyvalues(self):
-        self.process_params(64, 0, 0x10FFFF)
+        self.verify_key(64, 0, 0x10FFFF)
