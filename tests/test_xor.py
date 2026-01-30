2d0e45c0c80a6440c37e320d3afe0267cae65ad9
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 18:44:36 -0600

    Renaming method from process to encrypt_twice

--- tests/test_xor.py
+++ tests/test_xor.py
@@ -18,15 +18,15 @@ class TestXor(TestCase):
         with open(specialchars_path, 'r') as file:
             cls.specialchars = file.read()
 
-    def process(self, content, key):
+    def encrypt_twice(self, content, key):
         assert xor(xor(content, key), key) == content
 
     def test_message(self):
-        self.process(self.message, self.default_key)
-        self.process(self.message, self.key1)
-        self.process(self.message, self.key2)
+        self.encrypt_twice(self.message, self.default_key)
+        self.encrypt_twice(self.message, self.key1)
+        self.encrypt_twice(self.message, self.key2)
 
     def test_specialchars(self):
-        self.process(self.specialchars, self.default_key)
-        self.process(self.specialchars, self.key1)
-        self.process(self.specialchars, self.key2)
+        self.encrypt_twice(self.specialchars, self.default_key)
+        self.encrypt_twice(self.specialchars, self.key1)
+        self.encrypt_twice(self.specialchars, self.key2)
